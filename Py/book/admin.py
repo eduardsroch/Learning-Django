c6a41d587586dddbd9d345ea40ef7447cf4c2989
eduardsroch <eduardsroch@outlook.com>
2024-03-22 17:29:54 -0300

    Update

--- Py/book/admin.py
+++ Py/book/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from book.models import Livro
-from book.models import Autor
-from book.models import Editora
+from book.models import Descricao
 
 admin.site.register(Livro)
-admin.site.register(Autor)
-admin.site.register(Editora)
+admin.site.register(Descricao)
+
 
 
 
--- Py/book/models.py
+++ Py/book/models.py
@@ -9,18 +9,14 @@ class Livro (models.Model):
     edicao = models.IntegerField()
     paginas = models.IntegerField()
     preco = models.DecimalField(max_digits=5, decimal_places=2)
-    estoque = models.IntegerField()
+    estoque = models.IntegerField()    
     def __str__(self):
         return self.titulo
     
-class Autor (models.Model):
-    nome = models.CharField(max_length=100)
-    email = models.EmailField()
+class Descricao(models.Model):
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    descricao = models.TextField()
     def __str__(self):
-        return self.nome
-
-class Editora (models.Model):
-    nome = models.CharField(max_length=100)
-    def __str__(self):
-        return self.nome
+        return self.descricao
+    
     
